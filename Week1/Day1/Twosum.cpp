3235bbbbddcb856c09c2f7158a07c3281b31a0ee
AyushDabhodia <ashusingh5565@gmail.com>
2026-06-12 21:04:08 +0530

    Create Twosum.cpp

---
+++ Week1/Day1/Twosum.cpp
@@ -0,0 +1,20 @@
+lass Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> map;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            int required = target - nums[i];
+            
+            if (map.count(required)) {
+                return {map[required], i};
+            }
+            
+            map[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
