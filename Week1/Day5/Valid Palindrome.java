7da819ab3e8333e54413ccc07d0be8ddf2f37168
AyushDabhodia <ashusingh5565@gmail.com>
2026-06-14 09:59:33 +0530

    Update Valid Palindrome.java

--- Week1/Day5/Valid Palindrome.java
+++ Week1/Day5/Valid Palindrome.java
@@ -1,1 +1,9 @@
-
+public class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        
+        return cleaned.equals(reversed);
+    }
+}
