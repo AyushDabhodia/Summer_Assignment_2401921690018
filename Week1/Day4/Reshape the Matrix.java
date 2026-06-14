909c37f43576203bc39d2a28e672311e35e00fe5
AyushDabhodia <ashusingh5565@gmail.com>
2026-06-14 09:52:59 +0530

    Update Reshape the Matrix.java

--- Week1/Day4/Reshape the Matrix.java
+++ Week1/Day4/Reshape the Matrix.java
@@ -1,1 +1,32 @@
+public class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
 
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        int[] flatArray = new int[m * n];
+        int k = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                flatArray[k] = mat[i][j];
+                k++;
+            }
+        }
+
+        int[][] result = new int[r][c];
+        int index = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result[i][j] = flatArray[index];
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
