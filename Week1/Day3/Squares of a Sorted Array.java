850c2f672179fb73172408da09be5fb01fc28d7c
AyushDabhodia <ashusingh5565@gmail.com>
2026-06-14 09:42:53 +0530

    Update Squares of a Sorted Array.java

--- Week1/Day3/Squares of a Sorted Array.java
+++ Week1/Day3/Squares of a Sorted Array.java
@@ -1,1 +1,15 @@
+import java.util.Arrays;
 
+public class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * nums[i];
+        }
+        
+        Arrays.sort(result);
+        
+        return result;
+    }
+}
